5831d9f09a2508783654be8bc1333d521405c2cc
MarcusDunn <marcus.s.dunn@gmail.com>
2021-09-25 18:45:11 -0700

    gave up and added tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Stdio;
+use std::task::Context;
+use tokio::io::{AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tokio::process::Command;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let mut child = Command::new("python")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    child
+        .stdin
+        .take()
+        .expect("failed to take stdin from child")
+        .write_all("print(\"hello world\")\n".as_bytes())
+        .await?;
+
+    let mut string_out = String::new();
+
+    child
+        .stdout
+        .take()
+        .expect("failed to take stdout from child")
+        .read_to_string(&mut string_out)
+        .await?;
+
+    println!("look!: {}", string_out);
+
+    // Await until the command completes
+    let output = child.wait_with_output().await?;
+    println!("the command exited with: {:?}", output);
+    Ok(())
 }
