fcef86d942e2febef870b52d4a6bbae3291c121c
MarcusDunn <marcus.s.dunn@gmail.com>
2021-09-26 13:14:51 -0700

    poggers

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
-use std::process::Stdio;
-use std::task::Context;
-use tokio::io::{AsyncReadExt, AsyncWrite, AsyncWriteExt};
-use tokio::process::Command;
+#![feature(buf_read_has_data_left)]
 
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    let mut child = Command::new("python")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()?;
+use portable_pty::{native_pty_system, CommandBuilder, PtyPair, PtySize};
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 
-    child
-        .stdin
-        .take()
-        .expect("failed to take stdin from child")
-        .write_all("print(\"hello world\")\n".as_bytes())
-        .await?;
+fn main() -> Result<(), anyhow::Error> {
+    if let [_spice, prog, ..] = std::env::args().collect::<Vec<_>>().as_slice() {
+        let PtyPair { slave, mut master } = native_pty_system().openpty(PtySize::default())?;
 
-    let mut string_out = String::new();
+        let _ = slave.spawn_command(CommandBuilder::new(prog))?;
 
-    child
-        .stdout
-        .take()
-        .expect("failed to take stdout from child")
-        .read_to_string(&mut string_out)
-        .await?;
+        let mut reader = BufReader::new(master.try_clone_reader()?);
 
-    println!("look!: {}", string_out);
+        let write_handle = std::thread::spawn(move || {
+            let mut line = String::new();
+            loop {
+                reader.read_line(&mut line).expect("failed to read line");
+                print!("{}", line);
+                line.clear();
+            }
+        });
 
-    // Await until the command completes
-    let output = child.wait_with_output().await?;
-    println!("the command exited with: {:?}", output);
-    Ok(())
+        let read_handle = std::thread::spawn(move || {
+            let mut line = String::new();
+            loop {
+                std::io::stdin()
+                    .read_line(&mut line)
+                    .expect("failed to read line");
+                write!(master, "{}", line);
+                line.clear();
+            }
+        });
+
+        read_handle.join().expect("penis");
+        write_handle.join().expect("penis");
+
+        Ok(())
+    } else {
+        todo!()
+    }
 }
