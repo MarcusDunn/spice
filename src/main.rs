28333e0322311a6b96ece899a43ef163a7e0e754
MarcusDunn <marcus.s.dunn@gmail.com>
2021-09-26 18:05:36 -0700

    working, but broken

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,101 @@
-use std::io::{stdin, stdout, BufReader, Read, Write};
-use std::sync::mpsc::{channel, TryRecvError};
+use std::io::{stdin, stdout, BufReader, Write};
+use std::sync::mpsc::{channel, Sender};
 use std::thread;
-use std::thread::sleep;
+use std::thread::{sleep, JoinHandle};
 use std::time::Duration;
 
-use crossterm::{terminal, Command, ExecutableCommand};
+use crossterm::terminal;
+use crossterm::terminal::{Clear, ClearType};
 use portable_pty::{CommandBuilder, PtyPair, PtySize};
+use syntect::easy::HighlightLines;
+use syntect::highlighting::ThemeSet;
+use syntect::parsing::SyntaxSet;
+use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 
 fn main() -> Result<(), anyhow::Error> {
     let mut stdout = stdout();
-    let mut stdin = stdin();
+    let stdin = stdin();
 
     let PtyPair { slave, mut master } =
         portable_pty::native_pty_system().openpty(PtySize::default())?;
 
-    let repl = &std::env::args().collect::<Vec<_>>()[1];
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
 
-    let (master_tx, master_rx) = channel::<u8>();
-    let (stdin_tx, stdin_rx) = channel::<u8>();
+    if let [_spice, repl, args @ ..] = &std::env::args().collect::<Vec<_>>()[..] {
+        let mut repl = CommandBuilder::new(repl);
+        repl.args(args);
+        let mut child = slave.spawn_command(repl)?;
 
-    let mut child = slave.spawn_command(CommandBuilder::new(repl))?;
+        let master_buf_reader = BufReader::new(master.try_clone_reader()?);
+        let stdin_buf_reader = BufReader::new(stdin);
 
-    let mut master_buf_reader = BufReader::new(master.try_clone_reader()?);
-    let mut stdin_buf_reader = BufReader::new(stdin);
+        let (master_tx, master_rx) = channel::<u8>();
+        let (stdin_tx, stdin_rx) = channel::<u8>();
 
-    let master_reader_thread = thread::spawn(move || loop {
-        let mut buf = [0u8];
-        master_buf_reader
-            .read_exact(&mut buf)
-            .expect("failed to read byte");
-        master_tx.send(buf[0]).expect("failed to send char");
-    });
+        spawn_background_reader(master_tx, master_buf_reader);
+        spawn_background_reader(stdin_tx, stdin_buf_reader);
 
+        terminal::enable_raw_mode()?;
+
+        let mut current_line = String::new();
+
+        loop {
+            if let Some(exit_code) = child.try_wait()? {
+                write!(stdout, "child exited with {:?}", exit_code)?;
+                terminal::disable_raw_mode()?;
+                break;
+            } else {
+                while let Ok(c) = master_rx.try_recv() {
+                    let c = char::from(c);
+
+                    if c == '\n' {
+                        current_line.clear();
+                        writeln!(stdout)?;
+                    } else {
+                        crossterm::queue!(stdout, crossterm::cursor::MoveToPreviousLine(1))?;
+                        crossterm::queue!(
+                            stdout,
+                            crossterm::terminal::Clear(ClearType::CurrentLine)
+                        )?;
+                        stdout.flush()?;
+                        current_line.push(c);
+                        writeln!(stdout, "{}", highlight(&ps, &ts, current_line.clone()))?;
+                    }
+                }
+                while let Ok(c) = stdin_rx.try_recv() {
+                    master.write_all(&[c])?;
+                }
+                stdout.flush()?;
+                sleep(Duration::from_millis(50))
+            }
+        }
+
+        Ok(())
+    } else {
+        todo!()
+    }
+}
+
+fn highlight(ps: &SyntaxSet, ts: &ThemeSet, input: String) -> String {
+    let syntax = ps.find_syntax_by_extension("py").unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(&*input) {
+        let ranges = h.highlight(line, &ps);
+        return as_24_bit_terminal_escaped(&ranges[..], true);
+    }
+    todo!()
+}
+
+fn spawn_background_reader(
+    stdin_tx: Sender<u8>,
+    mut stdin_buf_reader: impl std::io::Read + Send + 'static,
+) -> JoinHandle<()> {
     thread::spawn(move || loop {
         let mut buf = [0u8];
         stdin_buf_reader
             .read_exact(&mut buf)
             .expect("failed to read byte");
         stdin_tx.send(buf[0]).expect("failed to send char");
-    });
-
-    terminal::enable_raw_mode()?;
-
-    'outer: loop {
-        if let Some(exit_code) = child.try_wait()? {
-            write!(stdout, "child exited with {:?}", exit_code)?;
-            break;
-        } else {
-            sleep(Duration::from_millis(100));
-        }
-        'master: loop {
-            match master_rx.try_recv() {
-                Ok(c) => {
-                    stdout.write_all(&[c])?;
-                }
-                Err(TryRecvError::Empty) => {
-                    break 'master;
-                }
-                Err(TryRecvError::Disconnected) => {
-                    break 'outer;
-                }
-            }
-        }
-        while let Ok(c) = stdin_rx.try_recv() {
-            master.write_all(&[c])?;
-        }
-        stdout.flush()?;
-    }
-
-    terminal::disable_raw_mode()?;
-
-    Ok(())
+    })
 }
